3d3823cb325179647f68a7fbc6a42a1ac4862695
Hercules Santos <hercules.eduardo@gmail.com>
2019-06-02 16:54:00 -0300

    adding dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-
 const app = express()
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('hello')
 })
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('running')
 })
