5a4d8fdd2a337dfb852bd89338290df645cbefbb
Hercules Santos <hercules.eduardo@gmail.com>
2019-06-02 17:45:37 -0300

    adding middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-require('dotenv').config()
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const HTTPStatus = require('http-status')
 const morgan = require('morgan')
 const logger = require('./helpers/logger')({ ctx: 'Application' })
 
 const port = process.env.PORT || 3000
+
+app.use(cors({
+  origin: [process.env.CORS_DOMAINS_ALLOWED],
+  credentials: true
+}))
+
+app.use((err, req, res, next) => {
+  res.status(err.status || HTTPStatus.INTERNAL_SERVER_ERROR)
+  if (!err.code) {
+    console.trace(err)
+  }
+  res.json(err)
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+app.use(cookieParser())
 
 app.use(morgan('dev', {
   skip: function (req, res) {
